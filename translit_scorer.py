3dd04a175f27d5de338f40dcd00f0d79dc904b4c
ngohgia <ngohoanggia@gmail.com>
2016-04-12 16:35:49 +0800

    Use sclite to score differences on syllabic level

--- translit_scorer.py
+++ translit_scorer.py
@@ -5,12 +5,26 @@ import os
 
 if __name__ == '__main__':
   try:
-    script, sclitePath, outputPath, refPath = sys.argv
+    script, sclitePath, outputPath, refPath, refLangSpecsPath = sys.argv
   except ValueError:
-    print "translit_scorer.py\tsclitPath\toutputPath\trefPath"
+    print "translit_scorer.py\tsclitePath\toutputPath\trefPath\trefLangSpecs"
     sys.exit(1)
 
 SYL_DELIM = '.'
+SUBSYL_DELIM = ' '
+ONSET = 'CONSO'
+NUCLEUS = 'NUCLEUS'
+CODA = 'CODA'
+TONE = 'TONE'
+
+#---------------------------------------------------------------------------#
+def readLangSpecs(specPath):
+  langSpecs = {}
+  specFile = open(specPath, 'r')
+  for line in specFile:
+    parts = [part.strip() for part in line.split()]
+    langSpecs[parts[0]] = parts[1:]
+  return langSpecs
 
 #---------------------------------------------------------------------------#
 def getData(outputPath, refPath):
@@ -34,15 +48,31 @@ def getData(outputPath, refPath):
   return [output, ref];
 
 #--------------------------------------------------------------------------#
-def computeAllSylErrors(output, ref, sclitePath):
+def computeScliteScore(hyp, ref, outputDir):
+  os.system(sclitePath + " -r " + ref + \
+          " -h " + hyp + \
+          " -i wsj -o pra -O " + outputDir)
+
+#--------------------------------------------------------------------------#
+def computeAllSylErrors(output, ref, sclitePath, langSpecs):
   sylErrorDict = {}
   
   baseDir = os.path.dirname(os.path.abspath(__file__))
   tmpDir = os.path.join(baseDir, 'tmp')
 
-  if os.path.exists(tmpDir):
-    os.remove(os.path.join(tmpDir, 'tmp_output_syls.txt'))
-    os.remove(os.path.join(tmpDir, 'tmp_ref_syls.txt'))
+  tmpOutput = os.path.join(tmpDir, 'tmp_output_syls.txt')
+  tmpRef    = os.path.join(tmpDir, 'tmp_ref_syls.txt')
+  outputTones = os.path.join(tmpDir, 'tmp_output_tones.txt')
+  refTones = os.path.join(tmpDir, 'tmp_ref_tones.txt')
+
+  if os.path.exists(tmpOutput):
+    os.remove(tmpOutput)
+  if os.path.exists(tmpRef):
+    os.remove(tmpRef)
+  if os.path.exists(outputTones):
+    os.remove(outputTones)
+  if os.path.exists(refTones):
+    os.remove(refTones)
 
   for i in range(len(output)):
     outputEntry = output[i];
@@ -54,27 +84,47 @@ def computeAllSylErrors(output, ref, sclitePath):
     idx = 1
     for oSyl in outputSyls:
       for rSyl in refSyls:
-        entry = (oSyl, rSyl)
-        writeTmpSylFiles(idx, oSyl, rSyl)
+        writeTmpSylFiles(idx, oSyl, rSyl, langSpecs)
         idx = idx + 1
 
+  computeScliteScore(tmpOutput, tmpRef, tmpDir)
+
 #-------------------------------------------------------------------------#
-def writeTmpSylFiles(idx, oSyl, rSyl):
+def writeTmpSylFiles(idx, oSyl, rSyl, langSpecs):
   baseDir = os.path.dirname(os.path.abspath(__file__))
   tmpDir = os.path.join(baseDir, 'tmp')
 
   if not os.path.exists(tmpDir):
     os.makedirs(tmpDir);
-  
+ 
+  oParts = [part.strip() for part in oSyl.split(SUBSYL_DELIM)]
+  if oParts[-1] in langSpecs[TONE]:
+    oTone = oParts[-1]
+    oParts = oParts[:-1]
+  else:
+    oTone = ''
+
+  rParts = [part.strip() for part in rSyl.split(SUBSYL_DELIM)]
+  if rParts[-1] in langSpecs[TONE]:
+    rTone = rParts[-1]
+    rParts = rParts[:-1]
+  else:
+    rTone = ''
+
   outputSylsFile = open(os.path.join(tmpDir, 'tmp_output_syls.txt'), 'a')
   refSylsFile = open(os.path.join(tmpDir, 'tmp_ref_syls.txt'), 'a')
+  outputTonesFile = open(os.path.join(tmpDir, 'tmp_output_tones.txt'), 'a')
+  refTonesFile = open(os.path.join(tmpDir, 'tmp_ref_tones.txt'), 'a')
   
-  outputSylsFile.write(oSyl + "\t\t(" + str(idx) + ")\n") 
-  refSylsFile.write(rSyl + "\t\t(" + str(idx) + ")\n")
+  outputSylsFile.write(SUBSYL_DELIM.join(oParts) + "\t\t(" + str(idx) + ")\n") 
+  refSylsFile.write(SUBSYL_DELIM.join(rParts) + "\t\t(" + str(idx) + ")\n")
+  outputTonesFile.write(oTone + "\n") 
+  refTonesFile.write(rTone + "\n")
 
   outputSylsFile.close()
   refSylsFile.close()
 
 [output, ref] = getData(outputPath, refPath)
-computeAllSylErrors(output, ref, sclitePath)
+langSpecs = readLangSpecs(refLangSpecsPath)
+computeAllSylErrors(output, ref, sclitePath, langSpecs)
 
